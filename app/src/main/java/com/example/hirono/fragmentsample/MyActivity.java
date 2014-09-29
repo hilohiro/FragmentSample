26ccac0f05f841c3da670ddd8cf9667e28feb598
hilohiro <hilohiro+bitbucket@gmail.com>
2014-09-30 00:39:34 +0900

    Activity、Fragmentのイベントにログを追加。

--- app/src/main/java/com/example/hirono/fragmentsample/MyActivity.java
+++ app/src/main/java/com/example/hirono/fragmentsample/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,9 +16,14 @@ import android.os.Build;
 
 public class MyActivity extends Activity {
 
+    private void d(final String msg) {
+        Log.d("MyActivity", "" + this.hashCode() + " - " + msg);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        d("onCreate()");
         setContentView(R.layout.activity_my);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -32,6 +38,42 @@ public class MyActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        d("onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        d("onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        d("onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        d("onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        d("onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        d("onDestroy()");
     }
 
     @Override
@@ -57,8 +99,85 @@ public class MyActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
+            d("onCreateView()");
             View rootView = inflater.inflate(R.layout.fragment_my, container, false);
             return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            d("onAttach()");
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            d("onCreate()");
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            d("onViewCreated()");
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            d("onActivityCreated()");
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            d("onStart()");
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            d("onResume()");
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            d("onSaveInstanceState()");
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            d("onPause()");
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            d("onStop()");
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            d("onDestroyView()");
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            d("onDestroy()");
+        }
+
+        @Override
+        public void onDetach() {
+            super.onDetach();
+            d("onDetach()");
+        }
+
+        private void d(final String msg) {
+            Log.d("Fragment", "" + this.hashCode() + " - " + msg);
         }
     }
 }
